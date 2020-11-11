e2349a47c1269325f54015f934af29cc85f25428
Rojan Jose <rojanjose@Rojans-MBP.attlocal.net>
2020-11-10 22:44:04 -0500

    file storage lab changes

--- src/common/models/entry.js
+++ src/common/models/entry.js
@@ -5,7 +5,7 @@ module.exports = function(Entry) {
 
     Entry.greet = function(msg, cb) {
         console.log("testing " + msg);
-        fs.appendFile('data.txt', msg+"\n", function (err) {
+        fs.appendFile('data/cache.txt', msg+"\n", function (err) {
             if (err) throw err;
             console.log('Saved!');
         }); 
