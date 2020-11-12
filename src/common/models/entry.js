61da696a39f5259d208b30391d0b541df44a3448
Rojan Jose <rojanjose@Rojans-MBP.attlocal.net>
2020-11-11 22:54:35 -0500

    lab1 updates

--- src/common/models/entry.js
+++ src/common/models/entry.js
@@ -1,14 +1,22 @@
 'use strict';
 var fs = require('fs');
+var debug = require('fs');
 
 module.exports = function(Entry) {
 
     Entry.greet = function(msg, cb) {
-        console.log("testing " + msg);
+
+        // console.log("testing " + msg);
+        fs.appendFile('logs/debug.txt', "Recevied message: "+ msg +"\n", function (err) {
+            if (err) throw err;
+            console.log('Debug stagement printed');
+        });
+
         fs.appendFile('data/cache.txt', msg+"\n", function (err) {
             if (err) throw err;
-            console.log('Saved!');
-        }); 
+            console.log('Saved in cache!');
+        });
+
         cb(null, 'Greetings... ' + msg);
       }
   
